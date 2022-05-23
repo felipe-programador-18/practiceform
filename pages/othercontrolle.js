d54c806b71a6fccb77ef37f5f052492a5fb563c6
Felipe <machadofelipe2016@outlook.com>
2022-05-23 19:06:23 -0300

    c4-create structure about code form

--- pages/othercontrolle.js
+++ pages/othercontrolle.js
@@ -1,9 +1,31 @@
 import React,{useState} from 'react'
 
 const creatControllForm = () => {
-    return(<>
-    
-    </>)
+    const [form, setform] = useState({
+        name:'',
+        email:'',
+        uf: ''
+    })
+
+    const onChange = event =>{
+     setform(event.target.value)
+    }
+
+
+    return(<> <h1>Create another page to practice about form controlled!!</h1>       
+            Name:
+             <input type='text' name='name' value={form.name} /> <br/>
+                        
+            Email:
+             <input type='text' name='email' value={form.email} /> <br/>
+
+             <button onChange={onChange}>Get State</button>
+             <button onChange={onChange}>Hide State</button>
+             
+             <pre> {JSON.stringify(form)}</pre>
+
+             {form === 'Pj' && <p>Please adding your Pj</p>}            
+     </>)
 }
 
 export default creatControllForm
