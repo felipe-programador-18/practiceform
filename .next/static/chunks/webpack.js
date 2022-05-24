8e701dad8073808f36dcdfedf78a32b31070493d
Felipe <machadofelipe2016@outlook.com>
2022-05-24 15:17:55 -0300

    c10-create another example to verify useref

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "67c72efd15e8254f"; }
+/******/ 		__webpack_require__.h = function() { return "585195f9018af8be"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/usereff.js
+++ pages/usereff.js
@@ -1,10 +1,36 @@
-import React,{useRef} from 'react'
+import React,{useRef, useEffect, useState} from 'react'
+
+export const showCounter = ({ counter, identifier }) =>{ 
+ const render = useRef(identifier)
+  
+ useEffect(() => {
+  render.current = render.current +1
+ })
+   
+  return (
+      <p> counter : {counter} here inside // renders: {render.current} {identifier} </p>
+  )
+}
+
+
+
 
 const createInsig = () => {
+ // try learning about useRef
+ const mydiv = useRef(null)
+ const [counter, setcounter] = useState(0)
+
 
     return(
-        <h1>Learning about useRef</h1>
-    )
+    <div  ref={mydiv}>
+     <h1>Learning about useRef</h1>
+     <h2>Pratice more about that!!</h2>
+
+     <button onClick={ ()=> setcounter(old => old+1) } >try code!!</button>
+     <showCounter counter={counter} identifier={0} />
+     <showCounter counter={10} identifier={10} />
+
+    </div>)
 }
 
 export default createInsig
