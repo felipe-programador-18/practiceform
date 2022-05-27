e06d5bac416f487238f68c5b3a22857cf88250db
Felipe <machadofelipe2016@outlook.com>
2022-05-27 15:07:08 -0300

    c13-adding more insight about uncontrolled form

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5a82bb2fdaaa5815"; }
+/******/ 		__webpack_require__.h = function() { return "eed908d06430c0a5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/otheruncro.js
+++ pages/otheruncro.js
@@ -1,17 +1,25 @@
 import React,{useRef, useState} from 'react'
 
 //practice little bit about form uncontrolled!!!
+//remember uncontrolled form is when
 const formUncot = () => {
-    const myReffer = useRef()
-    const [ show, setshow] = useState()
+    const myReffer = useRef(true)
+    const [show, setshow] = useState()
     
-    const handCons = evt =>{
-        console.log()
+    const handCons = ()=>{
+        console.log(myReffer?.current?.value)
     }
 
     return(<>
-         <input onClick={handCons} type={'text'} />
-         <input type={'text'} />
+    <div className='d-flex text-center'>
+    <h1 className='bg-red-700'>Practice more about form uncontrolled!!!</h1>
+         {show && 
+         <input type={'text'}   ref={myReffer} />
+              }
+         <button onClick={handCons}> Get State</button>
+         <button onClick={() => setshow(curr => !curr)} >Hide State</button>
+    
+         </div>
     </>)
 }
 
