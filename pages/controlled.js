a37ca6b38ba87e09f32181fd9168df5d6e88751b
Felipe <machadofelipe2016@outlook.com>
2022-05-27 19:47:29 -0300

    c14-practice little bit about form with next and react

--- pages/controlled.js
+++ pages/controlled.js
@@ -1,9 +1,10 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 
 const controForm = () => {
    const [value, setvaleu] = useState('')
    const [show, setshow] = useState(true)
-    const getState = () =>{
+   const [test, settest] = useState(true) 
+   const getState = () =>{
      console.log(value)
    }
    // inside functions i have passed it setvalue to get event of input!!
@@ -16,12 +17,21 @@ const controForm = () => {
    } 
 
    return(<>
+   <div className='d-flex text-center'>
         <h1>training about form uncontrolled!!</h1>
        { show && <input type='text' placeholder='Controlled' value={value}  onChange={onChange}  /> }
-        <button onClick={getState}>Get value</button>
-        <button onClick={()=> setshow(curr => !curr)}>hide value</button>
+        <button className='bg-green-600' onClick={getState}>Get value</button>
+        <button className='bg-yellow-500'  onClick={()=> setshow(curr => !curr)}>hide value</button>
         {value}
         {value === '' && <p>fielt invalidated!!</p>}
+
+       { test &&
+        <div> 
+          <p>testing some things!!</p>
+          <button className='bg-blue-700' onClick={() => settest(curr => !curr)  }  >Hide div</button>
+        </div>
+         }
+ </div>
  </>)
 }
 
